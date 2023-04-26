b5c73335120d4933f11f5ef4926d4506c086c8ef
jiachoi1 <choij698@newschool.edu>
2023-04-26 12:26:08 -0400

    revise1

---
+++ elastic-collection2/data/menu.js
@@ -0,0 +1,35 @@
+export default {
+  koreanDramaArchive: `<h2 class="menu_section koreanDramaArchive">
+    <span class="delay_2">K</span><span class="mainTxt_2">o</span
+    ><span class="mainTxt_1">r</span><span class="mainTxt_3">e</span
+    ><span class="delay_1">a</span><span class="mainTxt_2">n</span>
+    <span class="mainTxt_1">D</span><span class="mainTxt_3">r</span
+    ><span class="delay_3">a</span><span class="mainTxt_1">m</span
+    ><span class="delay_2">a</span><br class="mobile" /> <span class="delay_1">A</span
+    ><span class="mainTxt_1">r</span><span class="mainTxt_3">c</span
+    ><span class="mainTxt_4">h</span><span class="delay_1">i</span
+    ><span class="mainTxt_1">v</span><span class="mainTxt_2">e</span>
+  </h2>`
+  ,
+  menu1: `<h2 class="menu_section menu1">
+    <span class="delay_1">G</span><span class="mainTxt_1">e</span
+    ><span class="mainTxt_2">n</span><span class="delay_2">r</span
+    ><span class="mainTxt_3">e</span>
+  </h2>`,
+  menu2: `<h2 class="menu_section menu2">
+    <span class="delay_1">W</span><span class="mainTxt_1">h</span
+    ><span class="mainTxt_2">a</span><span class="mainTxt_4">t</span>
+    <span class="delay_2">M</span><span class="mainTxt_1">o</span
+    ><span class="mainTxt_3">o</span><span class="delay_1">d</span><br class="mobile" />
+    <span class="mainTxt_1">A</span><span class="mainTxt_3">r</span
+    ><span class="delay_1">e</span> <span class="mainTxt_3">Y</span
+    ><span class="mainTxt_2">o</span><span class="mainTxt_1">u</span>
+    <span class="mainTxt_1">I</span><span class="mainTxt_2">n</span>
+  </h2>`,
+  menu3: `<h2 class="menu_section menu3">
+    <span class="delay_1">S</span><span class="mainTxt_1">p</span
+    ><span class="mainTxt_3">o</span><span class="delay_2">i</span
+    ><span class="mainTxt_3">l</span><span class="delay_1">e</span
+    ><span class="mainTxt_2">r</span><span class="mainTxt_1">s</span>
+  </h2>`,
+};
